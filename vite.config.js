06fe193f95e6593d69d0325f7f2999ed6c23b093
Rahat <mrahatcreations@gmail.com>
2026-08-30 17:58:11 +0600

    fix: downlevel JS to chrome58/es2015 for universal Android WebView compatibility

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: ['chrome58', 'es2015'],
+    cssTarget: 'chrome61'
+  }
 })
